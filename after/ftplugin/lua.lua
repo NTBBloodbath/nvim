e6b607c9842e7c0190d394cc309ebabce6a103ba
NTBBloodbath <bloodbathalchemist@protonmail.com>
2022-03-31 09:57:09 -0400

    cleanup: remove `after/ftplugin` lua files

--- after/ftplugin/lua.lua
+++
@@ -1,5 +0,0 @@
--- :fennel:1648729467
-vim.opt["tabstop"] = 2
-vim.opt["shiftwidth"] = 2
-vim.opt["softtabstop"] = 2
-return nil
--- after/ftplugin/norg.lua
+++
@@ -1,7 +0,0 @@
--- :fennel:1648729524
-vim.opt["tabstop"] = 2
-vim.opt["shiftwidth"] = 2
-vim.opt["softtabstop"] = 2
-vim.opt["concealcursor"] = "n"
-vim.opt["textwidth"] = 120
-return nil
