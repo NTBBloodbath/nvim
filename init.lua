0b56c2078ee7d244e7b3b6059162814bcd26b228
NTBBloodbath <bloodbathalchemist@protonmail.com>
2022-08-23 20:38:25 -0400

    feat(init): use xpcall to require core module so we can return a stack traceback in case something goes wrong

--- init.lua
+++ init.lua
@@ -48,7 +48,10 @@ vim.defer_fn(function()
 	vim.api.nvim_command("rshada!")
 
 	-- Load configuration core
-	pcall(require, "core")
+	local loaded_core, core_err = xpcall(require, debug.traceback, "core")
+	if not loaded_core then
+	    vim.notify(string.format("There was an error requiring 'core'. Traceback:\n%s", core_err), vim.log.levels.ERROR)
+	end
 
 	-- Load user plugins
 	if is_installed("packer.nvim") then
