27369aeabbdc26baa26c3306e4942dbf53e045e7
NTBBloodbath <bloodbathalchemist@protonmail.com>
2022-10-20 19:01:32 -0400

    fix(lsp.config): resolve cmp-lsp deprecation warning

--- lua/modules/lsp/config.lua
+++ lua/modules/lsp/config.lua
@@ -125,7 +125,7 @@ local function on_attach(client, bufnr)
 end
 
 --- Capabilities
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.foldingRange = {
 	dynamicRegistration = true,
 	lineFoldingOnly = true,
