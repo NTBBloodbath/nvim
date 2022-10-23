3b0f0bd3dcf8c7167be5f3e6e67fc58c6edc9bde
NTBBloodbath <bloodbathalchemist@protonmail.com>
2022-10-22 21:46:10 -0400

    feat(after): add ftplugin for Python

---
+++ after/ftplugin/python.lua
@@ -0,0 +1,8 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.conceallevel = 2
+vim.opt.colorcolumn = "120"
+vim.opt.foldmethod = "syntax"
