f854bb5914370f8e57bdfe41c2d2335426344e2d
NTBBloodbath <bloodbathalchemist@protonmail.com>
2023-06-03 02:29:53 -0400

    fix(modules.completion): proper loading order for snippets and cmp

--- lua/modules/completion.lua
+++ lua/modules/completion.lua
@@ -11,9 +11,106 @@ return {
   { "onsails/lspkind-nvim" },
 
   --- God bless snippets
-  { "rafamadriz/friendly-snippets", lazy = false },
   {
     "L3MON4D3/LuaSnip",
+    build = "make install_jsregexp",
+    dependencies = {
+      { "rafamadriz/friendly-snippets", lazy = false },
+      --- Please someone _more_ competent make a better and consistent completion engine
+      {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+          "hrsh7th/cmp-path",
+          "hrsh7th/cmp-buffer",
+          "hrsh7th/cmp-nvim-lsp",
+          "saadparwaiz1/cmp_luasnip",
+          "lukas-reineke/cmp-under-comparator",
+        },
+        config = function()
+          local cmp = require("cmp")
+          local types = require("cmp.types")
+          local under_compare = require("cmp-under-comparator")
+
+          local luasnip = require("luasnip")
+          local lspkind = require("lspkind")
+
+          local has_words_before = function()
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0
+              and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s")
+              == nil
+          end
+
+          cmp.setup({
+            preselect = types.cmp.PreselectMode.None,
+            snippet = {
+              expand = function(args) luasnip.lsp_expand(args.body) end,
+            },
+            window = {
+              completion = {
+                scrollbar = "║",
+              },
+              documentation = {
+                scrollbar = "║",
+              },
+            },
+            mapping = {
+              ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+              ["<C-f>"] = cmp.mapping.scroll_docs(4),
+              ["<C-e>"] = cmp.mapping.abort(),
+              ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                elseif has_words_before() then
+                  cmp.complete()
+                else
+                  fallback()
+                end
+              end, { "i", "s" }),
+              ["<S-Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
+                else
+                  fallback()
+                end
+              end, { "i", "s" }),
+              ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            },
+            sources = {
+              { name = "nvim_lsp" },
+              { name = "luasnip" },
+              { name = "path" },
+              { name = "buffer" },
+            },
+            formatting = {
+              fields = { cmp.ItemField.Kind, cmp.ItemField.Abbr, cmp.ItemField.Menu },
+              format = lspkind.cmp_format({ with_text = false }),
+            },
+            sorting = {
+              comparators = {
+                cmp.config.compare.offset,
+                cmp.config.compare.exact,
+                cmp.config.compare.score,
+                under_compare.under,
+                cmp.config.compare.kind,
+                cmp.config.compare.sort_text,
+                cmp.config.compare.length,
+                cmp.config.compare.order,
+              },
+            },
+            experimental = {
+              ghost_text = true,
+            },
+          })
+        end,
+      },
+    },
+
     config = function()
       local luasnip = require("luasnip")
       local loader = require("luasnip.loaders.from_vscode")
@@ -24,101 +121,7 @@ return {
       })
 
       --- Load snippets
-      loader.load()
-    end,
-  },
-
-  --- Please someone _more_ competent make a better and consistent completion engine
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-nvim-lsp",
-      "saadparwaiz1/cmp_luasnip",
-      "lukas-reineke/cmp-under-comparator",
-    },
-    config = function()
-      local cmp = require("cmp")
-      local types = require("cmp.types")
-      local under_compare = require("cmp-under-comparator")
-
-      local luasnip = require("luasnip")
-      local lspkind = require("lspkind")
-
-      local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0
-          and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s")
-            == nil
-      end
-
-      cmp.setup({
-        preselect = types.cmp.PreselectMode.None,
-        snippet = {
-          expand = function(args) luasnip.lsp_expand(args.body) end,
-        },
-        window = {
-          completion = {
-            scrollbar = "║",
-          },
-          documentation = {
-            scrollbar = "║",
-          },
-        },
-        mapping = {
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        },
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path" },
-          { name = "buffer" },
-        },
-        formatting = {
-          fields = { cmp.ItemField.Kind, cmp.ItemField.Abbr, cmp.ItemField.Menu },
-          format = lspkind.cmp_format({ with_text = false }),
-        },
-        sorting = {
-          comparators = {
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            under_compare.under,
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
-          },
-        },
-        experimental = {
-          ghost_text = true,
-        },
-      })
+      loader.lazy_load()
     end,
   },
 }
