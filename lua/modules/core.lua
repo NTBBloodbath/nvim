93c71de288c5a1758ebfcf09a155fdd9a9a5bd72
NTBBloodbath <bloodbathalchemist@protonmail.com>
2023-06-03 02:37:50 -0400

    fix(modules.core): adjust neorg concealer config to latest changes

--- lua/modules/core.lua
+++ lua/modules/core.lua
@@ -135,16 +135,9 @@ return {
         },
         ["core.concealer"] = {
           config = {
-            markup_preset = "conceal",
             icons = {
               heading = {
-                enabled = true,
-                level_1 = { icon = "◈" },
-                level_2 = { icon = " ◆" },
-                level_3 = { icon = "  ◇" },
-                level_4 = { icon = "   ❖" },
-                level_5 = { icon = "    ⟡" },
-                level_6 = { icon = "     ⋄" },
+                icons = { "◈", "◆", "◇", "❖", "⟡", "⋄" },
               },
             },
             dim_code_blocks = {
