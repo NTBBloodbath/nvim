97aa8ae0b66a509c6b6ecc170b9b007d941eefb8
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-02-08 02:39:42 -0400

    feat(extern): tweak, fix and improve measure_nvim script

--- extern/measure_nvim.sh
+++ extern/measure_nvim.sh
@@ -1,41 +1,52 @@
 #!/usr/bin/env bash
 
 get_time() {
-    tail -1 tmp | cut -d ' ' -f 1
+    cat tmp | grep "NVIM STARTED" | cut -d ' ' -f 1
 }
 
 pf() {
     printf '%s : ' "$@"
 }
 
+# FIXME: I could not add >/dev/null at the end of each warmup as Neovim
+#        segfaults in WSL for some reason when redirecting to stdout lmao
 echo "Warmup #1"
-nvim -c q >/dev/null
+nvim -c q 2>&1 | tee -a /dev/null
 echo "Warmup #2"
-nvim -c q >/dev/null
+nvim -c q 2>&1 | tee -a /dev/null
 echo "Warmup #3"
-nvim -c q >/dev/null
+nvim -c q 2>&1 | tee -a /dev/null
 echo "Warmup #4"
-nvim -c q >/dev/null
+nvim -c q 2>&1 | tee -a /dev/null
 echo "Warmup #5"
-nvim -c q >/dev/null
+nvim -c q 2>&1 | tee -a /dev/null
 
 pf "No config"
 nvim --clean -nu NORC --startuptime tmp
 get_time
+rm tmp
+
 pf "With config"
 nvim --startuptime tmp
 get_time
+rm tmp
+
 pf "Opening init.lua"
 nvim "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/init.lua" --startuptime tmp
 get_time
+rm tmp
+
 pf "Opening Python file"
 nvim tmp.py --startuptime tmp
 get_time
+rm tmp
+
 pf "Opening C File"
 nvim tmp.c --startuptime tmp
 get_time
+rm tmp
+
 pf "Opening norg File"
 nvim tmp.norg --startuptime tmp
 get_time
-
 rm tmp
