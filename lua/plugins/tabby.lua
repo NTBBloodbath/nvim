26233560635f7095102b5c08ef58bf8042fca80d
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-02-10 04:21:30 -0400

    ref!: use `tabby.nvim` instead of `bufferline.nvim`

---
+++ lua/plugins/tabby.lua
@@ -0,0 +1,15 @@
+require("tabby.tabline").use_preset("active_wins_at_tail", {
+  theme = {
+    fill = "TabLineFill",
+    head = "TabLine",
+    current_tab = "TabLineSel",
+    tab = "TabLine",
+    win = "TabLine",
+    tail = "TabLine",
+  },
+  nerdfont = true,
+  lualine_theme = nil,
+  buf_name = {
+    mode = "unique",
+  }
+})
