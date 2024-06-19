5a4a682a84adea2978d9633aed9bc000d7fa6127
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-06-19 14:21:10 -0400

    feat: add daylight.nvim configuration file

---
+++ lua/plugins/daylight.lua
@@ -0,0 +1,11 @@
+require("daylight").setup({
+  day = {
+    name = "sweetie",
+    time = 8, -- 8 am
+  },
+  night = {
+    name = "sweetie",
+    time = 19, -- 7 pm, changes to dark theme on 07:01
+  },
+  interval = 60000, -- Time in milliseconds, 1 minute
+})
