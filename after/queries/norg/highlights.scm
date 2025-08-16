1a07be3fb8339c01a17f8fd299953bf55862be30
NTBBloodbath <bloodbathalchemist@protonmail.com>
2025-08-16 06:41:44 -0400

    feat: add more norg conceals

--- after/queries/norg/highlights.scm
+++ after/queries/norg/highlights.scm
@@ -1,9 +1,18 @@
 ;; extends
 
+;; Concealing queries for Norgolith-specific carryover tags
 ((weak_carryover_set
    (weak_carryover
      "_prefix" @prefix
      name: (tag_name) @name))
  (#match? @name "html.class")
  (#set! @prefix conceal "")
+ (#set! @name conceal ""))
+
+((weak_carryover_set
+   (weak_carryover
+     "_prefix" @prefix
+     name: (tag_name) @name))
+ (#match? @name "html.id")
+ (#set! @prefix conceal "")
  (#set! @name conceal ""))
